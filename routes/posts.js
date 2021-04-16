0d91c83b86261ae2beea331a154ef03cf50e32c8
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-04-16 16:45:36 +0600

    Added models and post, get routes for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
 import express from "express";
 
 import { getPosts } from "../controllers/posts.js";
+import { createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
+router.get("/", getPosts);
+router.post("/", createPost);
 router.get("/", getPosts);
 
 export default router;
